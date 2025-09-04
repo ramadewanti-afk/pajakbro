f2b9a252fdfa125503440e59c8bd22ce8ebb9c3b
rama dewanti <ramadewanti@gmail.com>
2025-09-04 08:51:50 +0000

    buat yang sederhana dulu yang penting tidak eror

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type {Metadata} from 'next';
 import './globals.css';
-import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
   title: 'Pajak Bro',
@@ -13,13 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-      </head>
-      <body className="antialiased">
-        {children}
-        <Toaster />
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
